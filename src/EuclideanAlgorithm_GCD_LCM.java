6524de5210312ee39fcccc0d17545031de89ae9a
sirin <s182211@outlook.com>
2023-02-03 15:51:10 +0100

    Calculation of GCD and LCM by Euclidean algorithm

--- src/EuclideanAlgorithm_GCD_LCM.java
+++ src/EuclideanAlgorithm_GCD_LCM.java
@@ -1,3 +1,9 @@
+/*
+ * The function gcd takes two integer parameters a and b and returns the GCD
+ * of a and b using the Euclidean algorithm. If b is equal to 0, the function returns a,
+ * which is the GCD of a and b. If b is not equal to 0, the function returns the result
+ * of gcd(b, a % b), which is the GCD of b and the remainder of the division of a by b.
+ */
 public class EuclideanAlgorithm_GCD_LCM {
     public static int gcd(int a, int b) {
         if (b == 0) {
