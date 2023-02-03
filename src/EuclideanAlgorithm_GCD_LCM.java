0de9832cdf42c268d0a941c20e1ba70815a62b64
sirin <s182211@outlook.com>
2023-02-03 15:48:09 +0100

    Math concept GCD and LCM

---
+++ src/EuclideanAlgorithm_GCD_LCM.java
@@ -0,0 +1,23 @@
+public class EuclideanAlgorithm_GCD_LCM {
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+        int a = 60;
+        int b = 48;
+        int gcd = gcd(a, b);
+        int lcm = lcm(a, b);
+        System.out.println("GCD of " + a + " and " + b + " is " + gcd);
+        System.out.println("LCM of " + a + " and " + b + " is " + lcm);
+    }
+}
+
